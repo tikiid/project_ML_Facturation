25481e668d413eec25f7186ffc1d879906590867
eliot ceron <49979791+el-cer@users.noreply.github.com>
2025-03-31 14:51:54 +0200

    Update OCR_Mistral.py

--- OCR_Mistral.py
+++ OCR_Mistral.py
@@ -13,7 +13,7 @@ import json
 import base64
 
 # Verify image exists
-image_file = Path("1001-receipt.jpg")
+image_file = Path("/Users/eliot/Desktop/project_ML/dataset/receipts/1127-receipt.jpg")
 assert image_file.is_file()
 
 # Encode image as base64 for API
@@ -28,4 +28,31 @@ image_response = client.ocr.process(
 # Convert response to JSON
 response_dict = json.loads(image_response.model_dump_json())
 json_string = json.dumps(response_dict, indent=4)
-print(json_string)
+
+image_ocr_markdown = response_dict["pages"][0]["markdown"]
+# Get structured response from model
+chat_response = client.chat.complete(
+    model="pixtral-12b-latest",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                ImageURLChunk(image_url=base64_data_url),
+                TextChunk(
+                    text=(
+                        f"This is image's OCR in markdown:\n\n{image_ocr_markdown}\n.\n"
+                        "I wan't same structured json with only 'name' restaurant final 'total' and 'date'"
+                        "The output should be strictly be json with no extra commentary"
+                        
+                    )
+                ),
+            ],
+        }
+    ],
+    response_format={"type": "json_object"},
+    temperature=0,
+)
+
+# Parse and return JSON response
+response_dict = json.loads(chat_response.choices[0].message.content)
+
