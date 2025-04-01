df8d30ffb163d1068eba8e6beaa25d1496d0a870
theaunaudin <150792197+theaunaudin@users.noreply.github.com>
2025-04-01 14:18:08 +0200

    Create fuzzy.py

---
+++ code/fuzzy.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from thefuzz import process
+
+df = pd.read_csv(r"C:\Users\theau\Desktop\HETIC\M2\ML\releve_final.csv")
+
+prix_cible = 25.00
+
+if "vendor" in df.columns and "price" in df.columns:
+    df_filtre = df[df["price"] == prix_cible]
+
+if "vendor" in df.columns:
+    bank_vendors = df["vendor"].dropna().unique().tolist()
+else:
+    print("La colonne 'vendor' n'existe pas dans le fichier.")
+    bank_vendors = []
+
+ticket_vendor = "Sukhothai Sushi Restaurant"
+
+if bank_vendors:
+    match, score = process.extractOne(ticket_vendor, bank_vendors)
+    
+    if score > 85:
+        print(f"Meilleure correspondance : {match} avec un score de {score}")
+    else:
+        print("Aucune correspondance fiable trouvée.")
+else:
+    print("Aucun vendeur à comparer.")
