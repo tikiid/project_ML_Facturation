82102ac119a6a80115d55a9bf8dbb2b5076d12a8
eliot ceron <49979791+el-cer@users.noreply.github.com>
2025-04-03 15:22:00 +0200

    Update fuzzy_join.py

--- application/fuzzy_join.py
+++ application/fuzzy_join.py
@@ -3,13 +3,23 @@ from thefuzz import process
 
 def join(df, target):
     
-    df_filtre = df[df["amount"] == target["amount"]]
+    df_filtre = df[df["amount"].round(1) == round(target["amount"], 1)]
     
     bank_vendors = df_filtre["vendor"].dropna().unique().tolist()
+    if len(bank_vendors) > 0:
     
-    match_str, score = process.extractOne(target["vendor"], bank_vendors)
-    match_df = df_filtre[df_filtre["vendor"] == match_str]
+        match_str, score = process.extractOne(target["vendor"], bank_vendors)
+        
+        if len(match_str) > 0:
+            match_df = df_filtre[df_filtre["vendor"] == match_str]
 
-    match_df["file_name"] = target["file_name"]
+            match_df.loc[:,"file_name"] = target["file_name"]
+            match_df.loc[:, "score"] = score
+            """match_df.loc[:, "pic_amount"] = target["amount"]
+            match_df.loc[:, "score"] = score
+            match_df.loc[:, "vendor_pic"] = target["vendor"]"""
+
+    else:
+        match_df = None
     
     return match_df
